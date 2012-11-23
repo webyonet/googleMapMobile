f0ed81252b6c5312f4af429321708715fa6233df
webyonet <webyonet@hotmail.com>
2012-11-23 10:14:52 +0200

    edit 1

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -1,11 +1,8 @@
 $(function(){
 	var url = null;
 	var stringBuilder = null;
-	$('a.menuLink').on('click',function(){
+	/*$('a.menuLink').on('click',function(){
 		url = $(this).attr('href');
-		/*stringBuilder = $.get('doc/' + url + '.html',function(data){
-			console.log($(data));	
-		});*/
 		$.ajax({
 			type: 'GET',
 			url:'doc/' + url + '.html',
@@ -17,5 +14,16 @@ $(function(){
 			}
 		});
 		return false;
+	});*/
+	
+	$('a.menuLink').on('click',function(){
+		url = $(this).attr('href');
+		$('aside.rightContent').empty().html($(url).html());
+		$('ul.ui-listview li').removeClass('active');
+		$(this).closest('li.ui-btn').addClass('active');
+		return false;
 	});
+	
+	
+	
 });
